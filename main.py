d319f97c9d98befd8dc871c300de8223e2cac488
AliMekky <https://github.com/AliMekky>
2022-03-23 15:04:01 +0200

    changing sizes

--- main.py
+++ main.py
@@ -91,7 +91,7 @@ def merge_sort(A,start,end): #start:first index of the array,end : last index in
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    sizes=[10,20,50,100]
+    sizes=[1000,2000,5000,10000]
     insertion_time=[]
     selection_time=[]
     merge_time=[]
